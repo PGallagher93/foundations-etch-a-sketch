03ae3f010416a53d7cb6374ba87da81986328bfa
Philip Gallagher <Pgallagher93@hotmail.com>
2023-04-07 19:41:44 +0100

    code tided up into functions, button now changes grid correctly

--- myscripts.js
+++ myscripts.js
@@ -1,48 +1,60 @@
-function makeGrid () {
-    sizePrompt =prompt("Enter a grid size between 1-100");
-    
-    if (sizePrompt === null){
-        return;
+
+ function createGrid (n) {
+    const containerDiv = document.querySelector('#gridContainer');
+    //for loop to make each row of the grid
+    for (i = 0; i < n; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('gridRow');
+        containerDiv.append(rowDiv);
+        
+    //loop for each column
+        for ( j = 0; j < n; j++){
+            const div = document.createElement('div');
+            div.classList.add('gridSquare');
+            rowDiv.append(div);
+            console.log("box added");
+        }
     }
-    
-    while (isNaN(parseInt(sizePrompt)) || (parseInt(sizePrompt) < 1) || (parseInt(sizePrompt) > 100)){
+    //color the squares
+    const squares = document.querySelectorAll('.gridSquare');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', ()=> {
+            square.classList.add('coloredSquare');
+        })
+    })
+    }
+
+    function makeGrid () {
+        sizePrompt =prompt("Enter a grid size between 1-64");
+        
         if (sizePrompt === null){
             return;
         }
-        sizePrompt = prompt("Grid size must be a number between 1-100");
+        
+        while (isNaN(parseInt(sizePrompt)) || (parseInt(sizePrompt) < 1) || (parseInt(sizePrompt) > 64)){
+            if (sizePrompt === null){
+                return;
+            }
+            sizePrompt = prompt("Grid size must be a number between 1-64");
+        }
+        gridSize = parseInt(sizePrompt);
+        //loop through current grid container and delete all grid squares
+        const gridContainer = document.querySelector("#gridContainer");
+        while (gridContainer.firstChild){
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+        createGrid(sizePrompt)
     }
-    gridSize = parseInt(sizePrompt);
+
+
+//load up the default grid size 
+window.onload = function (){
+    createGrid(16);
 }
-
-
-
-
-
-const containerDiv = document.querySelector('#gridContainer');
-//for loop to make each row of the grid
-for (i = 0; i < 16; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('gridRow');
-    containerDiv.append(rowDiv);
-    
-//loop for each column
-    for ( j = 0; j < 16; j++){
-        const div = document.createElement('div');
-        div.classList.add('gridSquare');
-        rowDiv.append(div);
-        console.log("box added");
-    }
-}
-//color the squares
-const squares = document.querySelectorAll('.gridSquare');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', ()=> {
-        square.classList.add('coloredSquare');
-    })
-})
-//button to control grid size
-
+//allow the button to create the grid size
 const sizeButton = document.querySelector('#gridSize') ;
 sizeButton.addEventListener('click', makeGrid);
+    
+    
 
 
