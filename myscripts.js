c26ab027c0017cc3af67ac308e7dbc506844bb28
Philip Gallagher <Pgallagher93@hotmail.com>
2023-04-07 11:21:03 +0100

    Title added, hover event added and Prompt for grid size added

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,14 @@
+function makeGrid () {
+    gridSize = parseInt(prompt("Enter a grid size between 1-100"));
+    while (isNaN(gridSize) || (gridSize < 1) || (gridSize > 100)){
+        gridSize = parseInt(prompt("Grid size must be a number between 1-100"));
+    }
+}
+
+
+
+
+
 const containerDiv = document.querySelector('#gridContainer');
 //for loop to make each row of the grid
 for (i = 0; i < 16; i++){
@@ -13,3 +24,16 @@ for (i = 0; i < 16; i++){
         console.log("box added");
     }
 }
+//color the squares
+const squares = document.querySelectorAll('.gridSquare');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', ()=> {
+        square.classList.add('coloredSquare');
+    })
+})
+//button to control grid size
+
+const sizeButton = document.querySelector('#gridSize') ;
+sizeButton.addEventListener('click', makeGrid);
+
+
